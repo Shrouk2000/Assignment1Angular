131143d4ca8282361dbb7df68fbcf4ef8078783f
shrouk2000 <sa2793@gayoum.edu.eg>
2025-08-10 16:57:09 +0300

    home route added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { About } from './components/about/about';
 import { Portfolio } from './components/portfolio/portfolio';
 import { Contact } from './components/contact/contact';
+import { Home } from './components/home/home';
 
 
 export const routes: Routes = [
+  {path:'', component:Home},
   { path: 'about', component: About },
   { path: 'portfolio', component: Portfolio },
   { path: 'contact', component: Contact },
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
-
+import 'zone.js';
 bootstrapApplication(App, {
   providers: [
     provideRouter(routes)
